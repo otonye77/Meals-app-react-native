7072cadfc0c671f1e3c8126cd4e8e2c9a5e68fb2
user <user@users-MacBook-Air.local>
2023-01-25 01:05:13 +0100

    master

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,20 @@
+import { FlatList } from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
+import { CATEGORIES } from "../data/dummy-data";
+
+function renderCategoryItem(itemData){
+    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+}
+
+const CategoriesScreen = () => {
+    return (
+        <FlatList 
+        key={'#'}
+        data={CATEGORIES}
+        renderItem={renderCategoryItem}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+        />
+    )
+}
+export default CategoriesScreen;
